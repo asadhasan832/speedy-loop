aba357e795d86d0e94d7b3799d6e90e7867cbc8d
Asad Hasan <asadhasan@Asads-MacBook-Pro.local>
2021-09-24 15:38:06 -0500

    Document Dijkstras Algorithm.

--- DijkstrasAlgorithm.java
+++ DijkstrasAlgorithm.java
@@ -10,24 +10,45 @@ class DijkstrasAlgorithm {
     private Set<Vertex> unvisitedVertices;
     private Map<Vertex, Long> distances;
 
+    /*
+	* Contructor to create Dijstras Algorithm instance for a graph.
+	* @param {Graph} graph
+	*/
     public DijkstrasAlgorithm(Graph graph) {
         this.graph = graph;
         this.vertices = graph.getVertices();
     }
 
+    /*
+	* Perform Dijkstra's traversal and construct a distances hashmap for each vertex.
+	* @param {Graph} graph
+	*/
     public void traverse(Vertex start) {
         distances = new HashMap<Vertex, Long>();
         unvisitedVertices = new HashSet<Vertex>();
 
+        //Start by setting the distance to the starting vertex to 0.
         distances.put(start, 0L);
+        //Add start vertex to unvisited vertices queue.
         unvisitedVertices.add(start);
         while(unvisitedVertices.size() > 0) {
+            //Find the vertex with the closest tenative distance
             Vertex v = getShortestTenative(unvisitedVertices);
+            //Remove vertex from unvisited queue.
             unvisitedVertices.remove(v);
+            //Iterate over all edges of vertex, if the distance to it
+            //including the current distance is less than the stored distance
+            //set the stored index to the sum of current vertex distance and edge weight
+            //and mark vertex as unvisited.
             findShortestDistances(v);
         }
     }
 
+    /*
+	* Get the stored shortest distance.
+	* @param {Vertex} v
+    * @return {long} - Returns the shortest distance or the max numerical value akin to infiniti.
+	*/
     public long getShortestDistance(Vertex v) {
         Long distance = distances.get(v);
         if(distance == null) {
@@ -37,6 +58,13 @@ class DijkstrasAlgorithm {
         }
     }
 
+    /*
+	* Get the distance between two vertices, or infinite (MAX_VALUE) if they have no,
+    * direct connection.
+	* @param {Vertex} start
+    * @param {Vertex} end
+    * @return {long} - Returns the distance between tow vertices.
+	*/
     private long getDistance(Vertex start, Vertex end) {
         List<Edge> edges = this.graph.getEdges(start);
         for(Edge edge: edges) {
@@ -45,6 +73,11 @@ class DijkstrasAlgorithm {
         return Long.MAX_VALUE;
     }
 
+    /*
+	* Get the vertiex closest amognst a given set of vertices.
+	* @param {Set<Vertex>} vertices
+    * @return {Vertex} - Returns closest vertex.
+	*/
     private Vertex getShortestTenative(Set<Vertex> vertices) {
         Vertex closest = null;
         for(Vertex v: vertices) {
@@ -59,6 +92,13 @@ class DijkstrasAlgorithm {
         return closest;
     }
 
+    /*Iterate over all edges of vertex, if the distance to it
+    * including the current distance is less than the stored distance
+    * set the stored index to the sum of current vertex distance and edge weight
+    * and mark vertex as unvisited. 
+    * @param {Vertex} - The vertex whose edges will be iterated to set distances for
+    * adjacent vertices, which are marked as unvisited if needed.
+    */
     private void findShortestDistances(Vertex v) {
         List<Edge> edges = this.graph.getEdges(v);
         for(Edge e: edges) {
